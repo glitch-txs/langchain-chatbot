f4a112cd731325cd04b63347c3f867b2ec5bac15
Glitch <glitch.txs@gmail.com>
2023-04-26 19:44:35 -0300

    first commit

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Input = (props: Props) => {
+  return (
+    <section>Input</section>
+  )
+}
+
+export default Input
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav>Navbar</nav>
+  )
+}
+
+export default Navbar
